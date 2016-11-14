b4a0887d33f7adf4db27b259b86665e2784551a4
SeboCode <mike-marti@outlook.com>
2016-11-15 00:28:52 +0100

    Added indexer

--- Matrix.Portable/Matrix.cs
+++ Matrix.Portable/Matrix.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix.Portable
 {
     public class Matrix
     {
+
+	    private int _columnCount;
+	    private int _rowCount;
+	    private double[][] _matrix;
+
+	    public double this[int column, int row]
+	    {
+		    get
+		    {
+			    if (column >= 0 && row >= 0 && column < _columnCount && row < _rowCount)
+				{
+					return _matrix[column][row];
+				}
+
+			    throw new ArgumentException(nameof(_matrix));
+		    }
+			set {
+				if (column >= 0 && row >= 0 && column < _columnCount && row < _rowCount)
+				{
+					_matrix[column][row] = value;
+				}
+			}
+		}
     }
 }
