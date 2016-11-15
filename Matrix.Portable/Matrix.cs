6095cb3fbd110bb01a4cf36064e8a95c0bc4242e
SeboCode <mike-marti@outlook.com>
2016-11-15 16:39:54 +0100

    Implemented multiplication Changed column and row Improved print

--- Matrix.Portable/Matrix.cs
+++ Matrix.Portable/Matrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Matrix.Portable
 {
@@ -7,78 +8,76 @@ namespace Matrix.Portable
 
 		private readonly double[][] _matrix;
 
-		public double this[int column, int row]
+		public double this[int row, int column]
 		{
 			get
 			{
-				if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
+				if (row < 0 || column < 0 || row >= RowCount || column >= ColumnCount)
 				{
 					throw new IndexOutOfRangeException(nameof(_matrix));
 				}
 
-				return _matrix[column][row];
+				return _matrix[row][column];
 			}
 			private set
 			{
-				if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
+				if (row < 0 || column < 0 || row >= RowCount || column >= ColumnCount)
 				{
 					throw new IndexOutOfRangeException(nameof(_matrix));
 				}
 
-				_matrix[column][row] = value;
+				_matrix[row][column] = value;
 			}
 		}
 
-		public Matrix(params double[][] columns)
+		public Matrix(params double[][] rows)
 		{
-			if (columns == null)
+			if (rows == null)
 			{
-				throw new ArgumentNullException(nameof(columns));
+				throw new ArgumentNullException(nameof(rows));
 			}
 
-			if (columns.Length < 1)
+			if (rows.Length < 1)
 			{
-				throw new ArgumentException(nameof(columns));
+				throw new ArgumentException(nameof(rows));
 			}
 
-			if (columns[0].Length < 1)
+			if (rows[0].Length < 1)
 			{
-				throw new ArgumentException(nameof(columns));
+				throw new ArgumentException(nameof(rows));
 			}
 
-			for (var i = 1; i < columns.Length; i++)
+			for (var i = 1; i < rows.Length; i++)
 			{
-				if (columns[i].Length != columns[i - 1].Length)
+				if (rows[i].Length != rows[i - 1].Length)
 				{
-					throw new ArgumentException(nameof(columns));
+					throw new ArgumentException(nameof(rows));
 				}
 			}
 
-			_matrix = new double[columns.Length][];
-			Array.Copy(columns, _matrix, columns.Length);
+			_matrix = new double[rows.Length][];
+			Array.Copy(rows, _matrix, rows.Length);
 		}
 
 		private Matrix(int columnSize, int rowSize)
 		{
 			_matrix = new double[columnSize][];
 
-			for (var i = 0; i < ColumnCount; i++)
+			for (var i = 0; i < RowCount; i++)
 			{
 				_matrix[i] = new double[rowSize];
 			}
 		}
 
-		public int ColumnCount => _matrix.Length;
+		public int RowCount => _matrix.Length;
 
-		public int RowCount => _matrix[0].Length;
+		public int ColumnCount => _matrix[0].Length;
 
-		public bool IsSquare => ColumnCount == RowCount;
+		public bool IsSquare => RowCount == ColumnCount;
 
 		private bool _isInverseMatrixCalculated = false;
 		private Matrix _inverseMatrix;
-
-		public Matrix InverseMatrix
-			=> _isInverseMatrixCalculated ? _inverseMatrix : (_inverseMatrix = CalculateInverseMatrix());
+		public Matrix InverseMatrix => _isInverseMatrixCalculated ? _inverseMatrix : (_inverseMatrix = CalculateInverseMatrix());
 
 		private Matrix CalculateInverseMatrix()
 		{
@@ -92,17 +91,17 @@ namespace Matrix.Portable
 
 		public static Matrix operator +(Matrix matrix, Matrix matrix2)
 		{
-			if (matrix.ColumnCount != matrix2.ColumnCount || matrix.RowCount != matrix2.RowCount)
+			if (matrix.RowCount != matrix2.RowCount || matrix.ColumnCount != matrix2.ColumnCount)
 			{
 				// todo implement own exception
 				throw new Exception();
 			}
 			
-			var returnMatrix = new Matrix(matrix.ColumnCount, matrix.RowCount);
+			var returnMatrix = new Matrix(matrix.RowCount, matrix.ColumnCount);
 
-			for (var i = 0; i < matrix.ColumnCount; i++)
+			for (var i = 0; i < matrix.RowCount; i++)
 			{
-				for (var x = 0; x < matrix.RowCount; x++)
+				for (var x = 0; x < matrix.ColumnCount; x++)
 				{
 					returnMatrix[i, x] = matrix[i, x] + matrix2[i, x];
 				}
@@ -111,15 +110,54 @@ namespace Matrix.Portable
 			return returnMatrix;
 		}
 
-		public static Matrix operator +(Matrix matrix, int skalar)
+		public static Matrix operator +(Matrix matrix, double skalar)
 		{
-			var returnMatrix = new Matrix(matrix.ColumnCount, matrix.RowCount);
+			var returnMatrix = new Matrix(matrix.RowCount, matrix.ColumnCount);
 
-			for (var i = 0; i < matrix.ColumnCount; i++)
+			for (var i = 0; i < matrix.RowCount; i++)
 			{
-				for (var x = 0; x < matrix.RowCount; x++)
+				for (var x = 0; x < matrix.ColumnCount; x++)
 				{
 					returnMatrix[i, x] = matrix[i, x] + skalar;
+				}
+			}
+
+			return returnMatrix;
+		}
+
+		public static Matrix operator *(Matrix matrix, Matrix matrix2)
+		{
+			if (matrix.RowCount != matrix2.ColumnCount)
+			{
+				// todo implement own exception
+				throw new Exception();
+			}
+
+			var returnMatrix = new Matrix(matrix.RowCount, matrix2.ColumnCount);
+
+			for (var i = 0; i < matrix.RowCount; i++)
+			{
+				for (var j = 0; j < matrix2.ColumnCount; j++)
+				{
+					for (var x = 0; x < matrix2.RowCount; x++)
+					{
+						returnMatrix[i, j] += matrix[i, x] * matrix2[x, j];
+					}
+				}
+			}
+
+			return returnMatrix;
+		}
+
+		public static Matrix operator *(Matrix matrix, double skalar)
+		{
+			var returnMatrix = new Matrix(matrix.RowCount, matrix.ColumnCount);
+
+			for (var i = 0; i < matrix.RowCount; i++)
+			{
+				for (var x = 0; x < matrix.ColumnCount; x++)
+				{
+					returnMatrix[i, x] += matrix[i, x] * skalar;
 				}
 			}
 
@@ -132,8 +170,12 @@ namespace Matrix.Portable
 
 			_matrix.ForEach(column =>
 			{
-				column.ForEach(element => str += $"{element} ");
-				str += "\r\n";
+				column.ForEach(element => str += $"{element}\t");
+
+				if (column != _matrix.Last())
+				{
+					str += "\r\n";
+				}
 			});
 
 			return str;
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -4,11 +4,57 @@ namespace Matrix
 {
 	public class Program
 	{
+		private readonly Portable.Matrix _matrix;
+		private readonly Portable.Matrix _matrix2;
+		private readonly Portable.Matrix _matrix3;
+
+		private Program()
+		{
+			_matrix = new Portable.Matrix(new double[] { 2, -1, 2 }, new double[] { -2, 3, 1 });
+			_matrix2 = new Portable.Matrix(new[] { 2.5, 3.4, 2.2 }, new[] { 8.5, 6.4, 4 });
+			_matrix3 = new Portable.Matrix(new double[] { -3, 2 }, new double[] { 2, 1 }, new double[] { 3, -1 });
+		}
+
+		public void PrintMatrix()
+		{
+			Console.WriteLine("Matrix1");
+			Console.WriteLine(_matrix);
+			Console.WriteLine();
+			Console.WriteLine("Matrix2");
+			Console.WriteLine(_matrix2);
+			Console.WriteLine();
+			Console.WriteLine("Matrix3");
+			Console.WriteLine(_matrix3);
+		}
+
+		public void PrintAdd()
+		{
+			Console.WriteLine("Add:");
+			Console.WriteLine("Matrix1 + 5 =");
+			Console.WriteLine(_matrix + 5);
+			Console.WriteLine();
+			Console.WriteLine("Matrix1 + Matrix2 =");
+			Console.WriteLine(_matrix + _matrix2);
+		}
+
+		public void PrintMulti()
+		{
+			Console.WriteLine("Multi");
+			Console.WriteLine("Matrix1 * 5 = ");
+			Console.WriteLine(_matrix * 5);
+			Console.WriteLine();
+			Console.WriteLine("Matrix1 * Matrix3 =");
+			Console.WriteLine(_matrix * _matrix3);
+		}
+
 		public static void Main(string[] args)
 		{
-			var matrix = new Portable.Matrix(new double[] { 2, 3, 2, 5 }, new double[] { 8, 6, 4, 9 });
-			var matrix2 = new Portable.Matrix(new[] { 2.5, 3.4, 2.2, 5.8 }, new[] { 8.5, 6.4, 4.1, 9 });
-			Console.WriteLine(matrix + 5);
+			var program = new Program();
+			program.PrintMatrix();
+			Console.WriteLine("------------------------------------");
+			program.PrintAdd();
+			Console.WriteLine("------------------------------------");
+			program.PrintMulti();
 			Console.ReadKey();
 		}
 	}
