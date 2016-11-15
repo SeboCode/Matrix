0727966c35147aa58801d801b0f6c1b3dfaabee5
SeboCode <mike-marti@outlook.com>
2016-11-15 09:26:09 +0100

    Improved indexer Implemented constructor Added some properties and some methods to matrix

--- Matrix.Portable/Matrix.cs
+++ Matrix.Portable/Matrix.cs
@@ -2,30 +2,78 @@
 
 namespace Matrix.Portable
 {
-    public class Matrix
+    public class Matrix : ICloneable
     {
-
-	    private int _columnCount;
-	    private int _rowCount;
+		
 	    private double[][] _matrix;
 
 	    public double this[int column, int row]
 	    {
 		    get
 		    {
-			    if (column >= 0 && row >= 0 && column < _columnCount && row < _rowCount)
+			    if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
 				{
-					return _matrix[column][row];
+					throw new IndexOutOfRangeException(nameof(_matrix));
 				}
 
-			    throw new ArgumentException(nameof(_matrix));
+			    return _matrix[column][row];
 		    }
-			set {
-				if (column >= 0 && row >= 0 && column < _columnCount && row < _rowCount)
+			set
+			{
+				if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
 				{
-					_matrix[column][row] = value;
+					throw new IndexOutOfRangeException(nameof(_matrix));
+				}
+
+				_matrix[column][row] = value;
+			}
+		}
+
+		public Matrix(params double[][] columns)
+		{
+			if (columns == null)
+			{
+				throw new ArgumentNullException(nameof(columns));
+			}
+
+			if (columns.Length < 1)
+			{
+				throw new ArgumentException(nameof(columns));
+			}
+
+			if (columns[0].Length < 1)
+			{
+				throw new ArgumentException(nameof(columns));
+			}
+
+			for (var i = 1; i < columns.Length; i++)
+			{
+				if (columns[i].Length != columns[i - 1].Length)
+				{
+					throw new ArgumentException(nameof(columns));
 				}
 			}
+
+			_matrix = new double[columns.Length][];
+			Array.Copy(columns, _matrix, columns.Length);
+		}
+
+	    public int ColumnCount => _matrix.Length;
+
+	    public int RowCount => _matrix[0].Length;
+
+		private bool _isInverseMatrixCalculated = false;
+		private Matrix _inverseMatrix;
+		public Matrix InverseMatrix => _isInverseMatrixCalculated ? _inverseMatrix : (_inverseMatrix = CalculateInverseMatrix());
+
+		private Matrix CalculateInverseMatrix()
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Clone()
+		{
+			throw new NotImplementedException();
 		}
     }
 }
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Matrix
+﻿using System;
+
+namespace Matrix
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
+			var matrix = new Matrix.Portable.Matrix(new double[] {2, 3, 2, 5}, new double[] {8, 6, 4, 9});
 		}
 	}
 }
