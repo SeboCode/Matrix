97eb3b130fb6da78dd4c50af46eb33e13f5bf924
SeboCode <mike-marti@outlook.com>
2016-11-15 12:55:56 +0100

    Removed generic cause of incompatibility with operator overloading Implemented + operator

--- Matrix.Portable/Matrix.cs
+++ Matrix.Portable/Matrix.cs
@@ -2,94 +2,141 @@
 
 namespace Matrix.Portable
 {
-	public class Matrix<T> : ICloneable
-		where T : struct, IComparable<T>
+	public class Matrix : ICloneable
 	{
 
-	private readonly T[][] _matrix;
+		private readonly double[][] _matrix;
 
-	public T this[int column, int row]
-	{
-		get
+		public double this[int column, int row]
 		{
-			if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
+			get
 			{
-				throw new IndexOutOfRangeException(nameof(_matrix));
+				if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
+				{
+					throw new IndexOutOfRangeException(nameof(_matrix));
+				}
+
+				return _matrix[column][row];
+			}
+			private set
+			{
+				if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
+				{
+					throw new IndexOutOfRangeException(nameof(_matrix));
+				}
+
+				_matrix[column][row] = value;
+			}
+		}
+
+		public Matrix(params double[][] columns)
+		{
+			if (columns == null)
+			{
+				throw new ArgumentNullException(nameof(columns));
 			}
 
-			return _matrix[column][row];
-		}
-		set
-		{
-			if (column < 0 || row < 0 || column >= ColumnCount || row >= RowCount)
-			{
-				throw new IndexOutOfRangeException(nameof(_matrix));
-			}
-
-			_matrix[column][row] = value;
-		}
-	}
-
-	public Matrix(params T[][] columns)
-	{
-		if (columns == null)
-		{
-			throw new ArgumentNullException(nameof(columns));
-		}
-
-		if (columns.Length < 1)
-		{
-			throw new ArgumentException(nameof(columns));
-		}
-
-		if (columns[0].Length < 1)
-		{
-			throw new ArgumentException(nameof(columns));
-		}
-
-		for (var i = 1; i < columns.Length; i++)
-		{
-			if (columns[i].Length != columns[i - 1].Length)
+			if (columns.Length < 1)
 			{
 				throw new ArgumentException(nameof(columns));
 			}
+
+			if (columns[0].Length < 1)
+			{
+				throw new ArgumentException(nameof(columns));
+			}
+
+			for (var i = 1; i < columns.Length; i++)
+			{
+				if (columns[i].Length != columns[i - 1].Length)
+				{
+					throw new ArgumentException(nameof(columns));
+				}
+			}
+
+			_matrix = new double[columns.Length][];
+			Array.Copy(columns, _matrix, columns.Length);
 		}
 
-		_matrix = new T[columns.Length][];
-		Array.Copy(columns, _matrix, columns.Length);
-	}
-
-	public int ColumnCount => _matrix.Length;
-
-	public int RowCount => _matrix[0].Length;
-
-	private bool _isInverseMatrixCalculated = false;
-	private Matrix<T> _inverseMatrix;
-
-	public Matrix<T> InverseMatrix
-		=> _isInverseMatrixCalculated ? _inverseMatrix : (_inverseMatrix = CalculateInverseMatrix());
-
-	private Matrix<T> CalculateInverseMatrix()
-	{
-		throw new NotImplementedException();
-	}
-
-	public object Clone()
-	{
-		throw new NotImplementedException();
-	}
-
-	public override string ToString()
-	{
-		var str = string.Empty;
-
-		_matrix.ForEach(column =>
+		private Matrix(int columnSize, int rowSize)
 		{
-			column.ForEach(element => str += $"{element} ");
-			str += "\r\n";
-		});
+			_matrix = new double[columnSize][];
 
-		return str;
-	}
+			for (var i = 0; i < ColumnCount; i++)
+			{
+				_matrix[i] = new double[rowSize];
+			}
+		}
+
+		public int ColumnCount => _matrix.Length;
+
+		public int RowCount => _matrix[0].Length;
+
+		public bool IsSquare => ColumnCount == RowCount;
+
+		private bool _isInverseMatrixCalculated = false;
+		private Matrix _inverseMatrix;
+
+		public Matrix InverseMatrix
+			=> _isInverseMatrixCalculated ? _inverseMatrix : (_inverseMatrix = CalculateInverseMatrix());
+
+		private Matrix CalculateInverseMatrix()
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Clone()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static Matrix operator +(Matrix matrix, Matrix matrix2)
+		{
+			if (matrix.ColumnCount != matrix2.ColumnCount || matrix.RowCount != matrix2.RowCount)
+			{
+				// todo implement own exception
+				throw new Exception();
+			}
+			
+			var returnMatrix = new Matrix(matrix.ColumnCount, matrix.RowCount);
+
+			for (var i = 0; i < matrix.ColumnCount; i++)
+			{
+				for (var x = 0; x < matrix.RowCount; x++)
+				{
+					returnMatrix[i, x] = matrix[i, x] + matrix2[i, x];
+				}
+			}
+
+			return returnMatrix;
+		}
+
+		public static Matrix operator +(Matrix matrix, int skalar)
+		{
+			var returnMatrix = new Matrix(matrix.ColumnCount, matrix.RowCount);
+
+			for (var i = 0; i < matrix.ColumnCount; i++)
+			{
+				for (var x = 0; x < matrix.RowCount; x++)
+				{
+					returnMatrix[i, x] = matrix[i, x] + skalar;
+				}
+			}
+
+			return returnMatrix;
+		}
+
+		public override string ToString()
+		{
+			var str = string.Empty;
+
+			_matrix.ForEach(column =>
+			{
+				column.ForEach(element => str += $"{element} ");
+				str += "\r\n";
+			});
+
+			return str;
+		}
 	}
 }
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -6,8 +6,9 @@ namespace Matrix
 	{
 		public static void Main(string[] args)
 		{
-			var matrix = new Portable.Matrix<int>(new[] {2, 3, 2, 5}, new[] {8, 6, 4, 9});
-			Console.WriteLine(matrix);
+			var matrix = new Portable.Matrix(new double[] { 2, 3, 2, 5 }, new double[] { 8, 6, 4, 9 });
+			var matrix2 = new Portable.Matrix(new[] { 2.5, 3.4, 2.2, 5.8 }, new[] { 8.5, 6.4, 4.1, 9 });
+			Console.WriteLine(matrix + 5);
 			Console.ReadKey();
 		}
 	}
